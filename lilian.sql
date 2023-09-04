597156f19487380cc4f6f44abcd32cd0b78f728c
Lilian de Jesus Correia <lilyhttpszzzz@gmail.com>
2023-09-04 00:30:15 -0300

    commit nº7

--- lilian.sql
+++ lilian.sql
@@ -29,3 +29,9 @@ from autores
 left join livros on autores.id = livros.autor_id
 group by autores.id
 order by autores.id;
+
+--7
+select curso, COUNT(aluno_id) as numero_de_alunos
+from matriculas
+group by curso
+order by curso;
