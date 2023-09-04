85ff3b4b6d0c0f1992f3d81ca856aa06736e9f70
Lilian de Jesus Correia <lilyhttpszzzz@gmail.com>
2023-09-03 21:57:28 -0300

    commit nº3

--- lilian.sql
+++ lilian.sql
@@ -5,3 +5,9 @@ select titulo from livros;
 
 --2
 select nome from autores where nascimento < '1900-01-01';
+
+--3
+select livros.titulo, autores.nome
+from livros 
+inner join autores on livros.autor_id = autores.id
+where autores.nome = 'J.K. Rowling';
