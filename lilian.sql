64fc1e01e5420c32a9e0593667bfef1b5091fbc8
Lilian de Jesus Correia <lilyhttpszzzz@gmail.com>
2023-09-09 23:46:33 -0300

    commit nº19

--- lilian.sql
+++ lilian.sql
@@ -109,3 +109,10 @@ from vendas
 group by produto
 order by menor_receita
 limit 1;
+
+--19
+select alunos.nome as aluno, COUNT(matriculas.id) as numero_de_matriculas
+from alunos
+left join matriculas on alunos.id = matriculas.aluno_id
+group by alunos.id
+order by aluno;
